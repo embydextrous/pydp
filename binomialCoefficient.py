bf488286e666001fefcebebd0f7be81286de7669
embydextrous <arjitagarwal099@gmail.com>
2020-10-04 20:20:18 +0530

    Binomial Coefficient

--- binomialCoefficient.py
+++ binomialCoefficient.py
@@ -1,3 +1,5 @@
+import time
+
 def binomialCoefficient(n, r):
     bc = [[0 for i in range(n + 1)] for j in range(n + 1)]
     bc[1][0] = 1
@@ -8,8 +10,44 @@ def binomialCoefficient(n, r):
                 bc[i][j] = 1
             else:
                 bc[i][j] = bc[i-1][j] + bc[i-1][j-1]
-    for row in bc:
-        print row
+            if i == n and j == r:
+                break
     return bc[n][r]
 
-print binomialCoefficient(24, 7)
+def binomialCoefficientSpaceOptimized(n, r):
+    bc = [[0 for i in range(n+1)] for j in range(2)]
+    bc[1][0] = 1
+    bc[1][1] = 1
+    for i in range(2, n+1):
+        bc[0] = bc[1][:]
+        for j in range(i+1):
+            if j == 0:
+                bc[1][j] = 1
+            else:
+                bc[1][j] = bc[0][j] + bc[0][j-1]
+            if i == n and j == r:
+                break
+    return bc[1][r]
+
+def binomialCoefficientSuperEfficient(n, r):
+    r = min(r, n - r)
+    result = 1
+    for i in range(r):
+        result *= (n - i)
+        result /= (i + 1)
+    return result
+
+start = time.time() * 1000
+print binomialCoefficient(1000, 375)
+end = time.time() * 1000
+print end - start
+
+start = time.time() * 1000
+print binomialCoefficientSpaceOptimized(1000, 375)
+end = time.time() * 1000
+print end - start
+
+start = time.time() * 1000
+print binomialCoefficientSuperEfficient(1000, 375)
+end = time.time() * 1000
+print end - start
